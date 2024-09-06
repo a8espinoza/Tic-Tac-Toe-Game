8da9845cf4196983d71006da97b6c893a342a688
aleja <aleja@AsusLaptop-Alex.lan>
2024-09-06 14:05:45 -0700

    multiplayer button functions, started on tile layout

--- TicTacToeGame/GameMenu.java
+++ TicTacToeGame/GameMenu.java
@@ -2,16 +2,21 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class GameMenu extends JFrame{
+public class GameMenu extends JFrame implements ActionListener{
 	
 	//panels and such
 	JFrame gameFrame;
-	JPanel gamePanel;
+	JPanel gameboard;
+	JButton a1, a2, a3, b1, b2, b3, c1, c2, c3;
+	JButton[] tiles = new JButton[9];
 	
 	//fonts and color management 
 	Font mainFont = new Font("Verdana", Font.PLAIN, 30);
@@ -26,7 +31,7 @@ public class GameMenu extends JFrame{
 		 //Generate Frame
 		gameFrame = new JFrame("Game");
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gameFrame.setSize(600, 625); //+25 for title bar
+		gameFrame.setSize(600, 620); //+25 for title bar
 		gameFrame.setLayout(null);
 		gameFrame.getContentPane().setBackground(menuGray);
 		gameFrame.setResizable(false);
@@ -34,13 +39,61 @@ public class GameMenu extends JFrame{
 		gameFrame.setVisible(true);
 		
 		//generate main game panel
-		gamePanel = new JPanel();
-		gamePanel.setBounds(50, 50, 100, 100);
-		gamePanel.setLayout(new GridLayout());
-		gamePanel.setBackground(darkGrayColor);
+		gameboard = new JPanel();
+		gameboard.setBounds(50, 90, 450, 450);
+		gameboard.setLayout(new GridLayout(3, 3, 10, 10));
+		gameboard.setBackground(darkGrayColor);
+		
+		//generate game tiles
+		a1 = new JButton(" ");
+		a2 = new JButton();
+		a3 = new JButton();
+		b1 = new JButton();
+		b2 = new JButton();
+		b3 = new JButton();
+		c1 = new JButton();
+		c2 = new JButton();
+		c3 = new JButton();
+		
+		tiles[0] = a1;
+		tiles[1] = b1;
+		tiles[2] = c1;
+		tiles[3] = a2;
+		tiles[4] = b2;
+		tiles[5] = c2;
+		tiles[6] = a3;
+		tiles[7] = b3;
+		tiles[8] = c3;
+		
+		for(int i=0; i<9; i++) {
+			tiles[i].addActionListener(this);
+			tiles[i].setFocusable(false);
+			tiles[i].setBackground(buttonLightGray);
+			tiles[i].setBorderPainted(false);
+		}
 
 		
 
+		//add tiles to panel, then panel to frame
+		gameboard.add(a1);
+		gameboard.add(b1);
+		gameboard.add(c1);
+		gameboard.add(a2);
+		gameboard.add(b2);
+		gameboard.add(c2);
+		gameboard.add(a3);
+		gameboard.add(b3);
+		gameboard.add(c3);
+
+		
+		gameFrame.add(gameboard);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
 
--- TicTacToeGame/MainMenu.java
+++ TicTacToeGame/MainMenu.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Frame;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -12,23 +13,27 @@ public class MainMenu implements ActionListener{
 	JFrame menuFrame;
 	JButton singlePlayer, multiPlayer;
 	JButton[] gameModes = new JButton[2];
+	Point location;
+
 	
 	
 	//font and color management
 	Font mainFont = new Font("Verdana", Font.PLAIN, 30);
 	Color menuGray = new Color(60,60,60);
-	Color buttonDarkGrayColor = new Color(30,30,30);
-	Color buttonTextWhite = new Color(200,200,200);
-	
+	Color buttonLightGray = new Color(90,90,90);
+	Color buttonTextBlack = new Color(5,5,5);
+	Color darkGrayColor = new Color(35,35,35); 	
 	
 	//constructor for main menu
 	MainMenu(){
 		menuFrame = new JFrame("Main Menu");
 		menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		menuFrame.setSize(600, 600);
+		menuFrame.setSize(600, 625); //+25 for title bar
+		menuFrame.setResizable(false);
 		menuFrame.setLayout(null);
 		menuFrame.getContentPane().setBackground(menuGray);
 		menuFrame.setVisible(true);
+		menuFrame.setLocationRelativeTo(null);
 		
 		
 		//menu buttons
@@ -37,10 +42,12 @@ public class MainMenu implements ActionListener{
 		gameModes[0] = singlePlayer;
 		gameModes[1] = multiPlayer;
 		for(int i = 0; i < 2; i++) {
-			gameModes[i].setBackground(buttonDarkGrayColor);
-			gameModes[i].setForeground(buttonTextWhite);
+			gameModes[i].setBackground(buttonLightGray);
+			gameModes[i].setForeground(buttonTextBlack);
 			gameModes[i].setFont(mainFont);
 			gameModes[i].addActionListener(this);
+			gameModes[i].setBorderPainted(false);
+			gameModes[i].setFocusable(false);
 		}
 		
 		singlePlayer.setBounds(100, 125, 400, 150);
@@ -53,12 +60,16 @@ public class MainMenu implements ActionListener{
 	
 	public static void main(String[] args) {
 		MainMenu menu = new MainMenu();
+		//GameMenu game = new GameMenu(null);
 
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(e.getSource() == multiPlayer) {
+			location = menuFrame.getLocation();
+			GameMenu gameMenu = new GameMenu(location);
+			menuFrame.setVisible(false); //disable for comparison
+		}
 	}
 
 }
